b4dee187cbcb2dd9d575a1fb57e77e2305216c17
Alan Balcom <alanbalcom@Alans-MacBook-Pro.local>
2021-05-13 00:40:50 -0400

    worked on js in study group

---
+++ index.js
@@ -0,0 +1,295 @@
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+
+const inquirer = require('inquirer');
+const render = require('./src/page-template');
+const fs = require('fs');
+const path = require('path');
+const output_dir = path.resolve(__dirname, "dist")
+const outputPath = path.join(output_dir, "team.html");
+
+const teamArray = [];
+const idArray = [];
+
+// function createTeam() {
+//     inquirer.prompt([
+//         {
+//             name: "name",
+//             type: "input",
+//             message: "Do you want to make a team?",
+//             validate: nameInput => {
+//                 if(nameInput) {
+//                     return true;
+//                 } else {
+//                     console.log('Is this your first time with a computer?')
+//                 }
+//             }
+//         },
+//         {
+//             name: "name",
+//             type: "input",
+//             message: "What is the persons name?"
+//         },
+//         {
+//             name: "id",
+//             type: "input",
+//             message: "What is the persons work id?"
+//         },
+//         {
+//             name: "email",
+//             type: "input",
+//             message: "What is the persons email?"
+//         },
+//                 {  
+//             name: "role",
+//             type: "list",
+//             message: "What does this person do at this corporate hellscape?",
+//             choices: ['Manager', 'Engineer', 'Intern', 'No one else to add']
+//         },
+//     ]) 
+//      //inquirer prompt
+//     //switch if user chose engineer call addengineer func, if chose intern call addintern func )
+// //do you want to create eng. intern, or done
+// }
+
+// function createManager() {
+//     //inquirer prompt
+//     createTeam() 
+// }
+
+// function addEngineer() {
+//     createTeam()
+// }
+
+// function addIntern() {
+//     createTeam()
+// }
+
+// function buildTeam() {
+//     fs.writeFileSync(outputPath, render(teamMembers), "utf-8");
+// }
+
+// createManager()
+// //use inquirer to create manager
+
+
+const appMenu = () => {
+    const addManager = () => {
+        return inquirer
+            .prompt([
+                {
+                    name: "name",
+                    type: "input",
+                    message: "What is the name of the manager of this team?",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true;
+                        } else {
+                            console.log("please enter the managers name to continue");
+                            return false;
+                        }
+                    }
+                },
+                {
+                    name: "id",
+                    type: "input",
+                    message: "Enter the managers ID number",
+                    validate: nameInput => {
+                        if (isNaN(nameInput)) {
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "please enter the managers email to continue",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true
+                        } else {
+                            console.log("please enter managers email to continue")
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "officeNumber",
+                    message: "what is the managers office number?",
+                    validate: nameInput => {
+                        if (isNaN(nameInput)) {
+                            console.log('please enter numbers to continue')
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                }
+            ])
+            .then(managerInput => {
+                const { name, id, email, officeNumber } = managerInput;
+                const manager = new Manager(name, id, email, officeNumber);
+                teamArray.push(manager);
+                idArray.push(id)
+                console.log(manager);
+                addEmployee();
+            })
+    };
+    const addEmployee = () => {
+        return inquirer.prompt([
+            {
+                type: "list",
+                name: "role",
+                message: "choose the employees role",
+                choices: ['engineer', 'intern', 'no one else to add']
+            }
+        ]).then(userChoice => {
+            switch (userChoice.role) {
+                case "engineer":
+                    addEngineer();
+                    break;
+                case "intern":
+                    addIntern();
+                    break;
+                default:
+                    buildTeam();
+            }
+        }
+        )}
+    const addEngineer = () => {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "what is the engineers name?",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log("please enter engineers name to continue")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "enter engineers ID number",
+                validate: nameInput => {
+                    if (isNaN(nameInput)) {
+                        console.log("please enter numbers to continue")
+                        return false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "please enter the engineers email to continue",
+                validate: emailInput => {
+                    if (emailInput) {
+                        return true
+                    } else {
+                        console.log("please enter engineers email to continue")
+                        return false;
+                    }
+                }
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "please enter the engineers github username to continue",
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true
+                    } else {
+                        console.log("please enter engineers github username to continue")
+                    }
+                }
+            },
+        ]).then(engineerInput => {
+            const { name, id, email, github } = engineerInput;
+            const engineer = new Engineer(name, id, email, github);
+            teamArray.push(engineer);
+            idArray.push(id)
+            console.log(engineer);
+            addEmployee();
+        })
+    }
+        const addIntern = () => {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "what is the interns name?",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true;
+                        } else {
+                            console.log("please enter interns name to continue")
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "enter interns ID number",
+                    validate: nameInput => {
+                        if (isNaN(nameInput)) {
+                            console.log("please enter numbers to continue")
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "please enter the interns email to continue",
+                    validate: emailInput => {
+                        if (emailInput) {
+                            return true
+                        } else {
+                            console.log("please enter interns email to continue")
+                            return false;
+                        }
+                    }
+                },
+                {
+                    type: "input",
+                    name: "school",
+                    message: "please enter the interns school to continue",
+                    validate: nameInput => {
+                        if (nameInput) {
+                            return true
+                        } else {
+                            console.log("please enter interns school username to continue")
+                        }
+                    }
+                },
+            ]).then(internInput => {
+                const { name, id, email, school } = internInput;
+                const intern = new Intern(name, id, email, school);
+                teamArray.push(intern);
+                idArray.push(id)
+                console.log(intern);
+                addEmployee();
+            })
+        };
+        const buildTeam = () => {
+            if (!fs.existsSync(output_dir)) {
+                fs.mkdirSync(output_dir)
+            }
+            fs.writeFileSync(outputPath, render(teamArray), 'utf-8')
+        }
+addManager();
+};
+appMenu();
